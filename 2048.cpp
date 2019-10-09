fbd144a97a771e9ba30e93ea45f0c5b762be14e5
Ben DeVries <bendevries66@gmail.com>
2019-10-08 20:06:50 -0400

    display a 10x10 screen

--- 2048.cpp
+++ 2048.cpp
@@ -1,11 +1,40 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
+const int width = 10;
+const int height = 10;
+int background[width*height];
+
 int main(){
+
+  /* INITIALIZE */
+  // determines terminal type, sets up ncurses environment
   initscr();
-  printw("Hello World");
+
+  // disables line buffering
+  cbreak();
+
+  // disables echoing input back to screen
+  noecho();
+
+  // clears the screen
+  clear();
+
+  int x, y;
+  for(y = 0; y < height; y++){
+    for(x = 0; x < width * 3; x += 3){
+      mvaddch(y, x, '0');
+      mvaddch(y, x+1, ' ');
+      mvaddch(y, x+2, ' ');
+    }
+  }
+
+  // display changes to the screen
   refresh();
+
+  // close program when the user presses a key
   getch();
   endwin();
+
   return 0;
 }
